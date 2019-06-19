4f03840227a6352e898d8d385f80a5ec5e09cfae
Raissa <raiarruda.raa@gmail.com>
2019-06-19 15:35:48 -0300

    add css

--- wp-content/themes/personalizado/index.php
+++ wp-content/themes/personalizado/index.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-       <!-- pra ter acessao painel -->
-        <?php wp_head(); ?> 
-        <style>
-            .teste {
-            width: 10rem;
-            height: 10rem;
-            object-fit: contain;
-            }
-        </style>
-
-    </head>
-    <body> 
+<?php get_header();?>
+    
     <?php 
         $args = array( 
         'orderby' => 'title',
